f7d67ab1862f4cfcfa3e135340dd8a069d013ad7
Krzysztof Wawrzkiewicz <chris.wawrz@gmail.com>
2022-07-04 11:02:04 +0200

    working login and register

---
+++ JobPlusPlusInitial/JobPlusPlusInitial/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using JobPlusPlusInitial.Areas.Identity.Data;
+using JobPlusPlusInitial.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(JobPlusPlusInitial.Areas.Identity.IdentityHostingStartup))]
+namespace JobPlusPlusInitial.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => { 
+            services.AddDbContext<JobPlusDBContext>(options =>
+                options.UseSqlServer(
+                    context.Configuration.GetConnectionString("JobPlusDBContextConnection")));
+                   services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<JobPlusDBContext>();
+            });
+
+
+        }
+    }
+}
